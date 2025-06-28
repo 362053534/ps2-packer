1684fda50123b3fac436007030e32bd3a299ddea
峰哥 <362053534@qq.com>
2025-06-28 15:02:22 +0800

    Revert "Merge pull request #42 from fjtrujy/gcc15"
    This reverts commit 2124267fe428fb312b5fb7767e186194f52f0b61, reversing
    changes made to fbee6f2801d38f5a4009cc773af92660472c7c78.

--- common/lzma/LzmaEnc.c
+++ common/lzma/LzmaEnc.c
@@ -2832,7 +2832,7 @@ SRes LzmaEnc_CodeOneMemBlock(CLzmaEncHandle pp, BoolInt reInit,
   CLzmaEnc *p = (CLzmaEnc *)pp;
   UInt64 nowPos64;
   SRes res;
-  static CLzmaEnc_SeqOutStreamBuf outStream;
+  CLzmaEnc_SeqOutStreamBuf outStream;
 
   outStream.vt.Write = SeqOutStreamBuf_Write;
   outStream.data = dest;
